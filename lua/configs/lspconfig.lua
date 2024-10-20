fae45c3cb892fbd1b53601065e3a4e0de7cf3cbe
sriram-moloco <sriram.ramesh@moloco.com>
2024-10-20 13:58:28 +0530

    Fix Lua server

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -8,7 +8,35 @@ local servers = {
   awk_ls = {},
   bashls = {},
   tsserver = {},
-  lua_ls = {},
+  lua_ls = {
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = "LuaJIT",
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = { "vim" },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
+          -- library = {
+          --   vim.env.VIMRUNTIME
+          --   -- Depending on the usage, you might want to add additional paths here.
+          --   -- "${3rd}/luv/library"
+          --   -- "${3rd}/busted/library",
+          -- },
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  },
   -- sqlls = {},
 
   pyright = {
